d7b7d902ff58c63e356b795f2f231a8fd163386a
Aboobacker MK <aboobackervyd@gmail.com>
2018-09-04 12:06:49 +0530

    Added files which were missing in previous commit

---
+++ app/services/fetch_user_from_token.rb
@@ -0,0 +1,27 @@
+class FetchUserFromToken
+  class MissingAuthHeader < StandardError; end
+  class InvalidToken < StandardError; end
+
+  def initialize(header)
+    @header = header
+  end
+
+  def call
+    raise MissingAuthHeader unless @header.present?
+    begin
+      token = JsonWebToken.decode(auth_header).first
+    rescue JWT::DecodeError => e
+      Rails.logger.info e.message
+      raise InvalidToken e.message
+    end
+    raise InvalidToken unless JsonWebToken.valid_payload(token)
+    User.find(token['user_id'])
+  end
+
+  private
+
+  attr_reader :header
+  def auth_header
+    header.split(' ').last
+  end
+end
